f47e2f5ae1c5057b64def9323af68753a9411f9f
Road-block <dridzt.addons@hotmail.com>
2017-11-28 23:33:14 +0200

    Initial

---
+++ Glass/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Glass
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        
+    }
+}
---
+++ Glass/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Glass
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            const float refresh = (float)1000 / 60;
+            var timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(refresh)
+            };
+            timer.Tick += TimerTick;
+            timer.Start();
+
+            void TimerTick(object sender, EventArgs e)
+            {
+                // something that redraws / repaints the main window
+                GlassImage.InvalidateVisual();
+                GlassImage.UpdateLayout();
+            }
+        }
+    }
+}
